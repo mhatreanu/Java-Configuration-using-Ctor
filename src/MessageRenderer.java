6c7b3e581d305e8068243579def3d8b208592ff6
Mhatre <anushree-shirish.mhatre@capgemini.com>
2019-04-10 18:36:58 +0530

    javaConfig

---
+++ src/MessageRenderer.java
@@ -0,0 +1,19 @@
+package com.capgemini.renderer;
+import com.capgemini.provider.MessageProvider;
+import com.capgemini.provider.implementer.HelloMessageProvider;
+import com.capgemini.provider.GmMessageProvider;
+
+	public class MessageRenderer{
+
+		private MessageProvider provider;
+
+		public MessageRenderer(MessageProvider provider){
+			
+			this.provider=provider;
+		}
+		public void render(){
+		
+			System.out.println(provider.printMessage());
+		}
+
+}
